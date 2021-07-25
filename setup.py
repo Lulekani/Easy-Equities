8626a8449c60a4cfb0761482b76cea669aae5cf1
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-07-25 17:36:08 +0200

    The first distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'EasyEquities',         # How you named your package folder (MyLib)
-  packages = ['Trading'],   # Chose the same as "name"
+  packages = ['EasyEquities'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'EasyEquities active scraper to issue trading instructions',   # Give a short description about your library
