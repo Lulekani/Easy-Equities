a203d0cd2da50214437cb137afa78245d2b36d34
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-07-26 11:31:42 +0200

    new disclaimer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'EasyEquities',         # How you named your package folder (MyLib)
   packages = ['EasyEquities'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'EasyEquities active scraper to issue trading instructions',   # Give a short description about your library
   author = 'Kloniphani Maxakadzi',                   # Type in your name
